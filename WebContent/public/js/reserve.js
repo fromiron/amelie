29fe36a3f6e6150f7ff5ffac8c7ff183e4ae71ee
Grayson Lee <artless1112@gmail.com>
2020-08-02 21:52:52 +0900

    Update reserve.js

--- WebContent/public/js/reserve.js
+++ WebContent/public/js/reserve.js
@@ -1,16 +1,12 @@
 function fillWhite(){
 	const otherTarget =  document.querySelectorAll(".reserveCourse");
-
 	for(i=0; i<otherTarget.length; i++){
 		otherTarget[i].classList.remove("selected");
 	}
 }
 
 window.onload = function() {
-
-
 	const reserveCourse =  document.querySelectorAll(".menu_list__menu_title");
-
 	const courseId =  document.getElementById("courseId");
 	const courseName =  document.getElementById("courseName");
 	const msgBox = document.querySelector("#reserve__msg");
@@ -23,10 +19,6 @@ for(i =0; i<reserveCourse.length;i++){
         parent.classList.add("selected");
 		courseName.value=parent.firstElementChild.nextElementSibling.textContent;
 		courseId.value=parent.firstElementChild.innerText;
-
 	});
-
 	}
-
-
 }
